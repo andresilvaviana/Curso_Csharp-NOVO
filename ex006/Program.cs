940dfbf082dbd8532279b24ab85355c3ca2cbc08
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-15 19:38:26 -0300

    Adicionar arquivos de projeto.

---
+++ ex006/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float nota1, nota2, nota3, resposta;
+
+        Console.Write("digite a nota 1:");
+        nota1 = float.Parse(Console.ReadLine());
+
+        Console.Write("digite a nota 2:");
+        nota2 = float.Parse(Console.ReadLine());
+
+        nota3 = nota1 + nota2;
+        resposta = nota3 / 2;
+
+        Console.WriteLine("a media das suas notas é:{0}", resposta);
+
+
+        double nota1, nota2, nota3, resposta;
+
+        Console.Write("digite a nota 1:");
+        nota1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("digite a nota 2:");
+        nota2 = Convert.ToDouble(Console.ReadLine());
+
+        nota3 = (nota1 + nota2) / 2;
+
+        Console.WriteLine("a media das suas notas é:{0}", nota3);
+
+
+
+
+
+
+    }
+}
