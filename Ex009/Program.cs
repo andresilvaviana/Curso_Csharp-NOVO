bab6f2698592078eb0650a67ef06518e1d76f62f
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-15 22:25:11 -0300

    distancia

---
+++ Ex009/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia, valor;
+            
+            Console.WriteLine("qual a distancia da sua casa ate o seu destino? ")
+            distancia = Convert.ToInt32(Console.ReadLine());
+
+            if (distancia >  )
+        }
+    }
+}
