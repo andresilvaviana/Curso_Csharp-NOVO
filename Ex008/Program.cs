f03291570c8dc980df1da7bc95075a50f860ee40
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-15 22:15:00 -0300

    Velocidade do carro.

---
+++ Ex008/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int velocidade, valor;
+            string multado;
+            
+
+            Console.WriteLine("Qual a velocidade que o seu veiculo estava? ");
+            velocidade = Convert.ToInt32(Console.ReadLine());
+
+            valor = (velocidade - 80) * 7;
+
+            if (velocidade > 80)
+
+            {
+                Console.WriteLine("Voce foi multado! Deve ser pago o valor de {0} referente aos km excedidos", valor);
+            }
+            else
+            {
+                Console.WriteLine("Voce dirigil com prudencia!");
+                
+            }
+
+           
+        }
+    }
+}
