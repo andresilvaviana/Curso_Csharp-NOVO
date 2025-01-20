f567f18558e8478f099b3fdbb02bbb430a03b398
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-20 19:30:16 -0300

    Projeto Triangulo

--- Ex0010/Ex0010/Program.cs
+++ Ex0010/Ex0010/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
+            int ladoA, ladoB, ladoC;
             
+            Console.Write("Digite o valor do lado A: ");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado B: ");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado C: ");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
 
         }
     }
