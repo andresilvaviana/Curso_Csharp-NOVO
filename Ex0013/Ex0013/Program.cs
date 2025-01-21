a1138342aab5d38235203841cbad030b93986e5b
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-20 22:23:03 -0300

    Valor por KM

---
+++ Ex0013/Ex0013/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex0013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int KM;
+
+            Console.WriteLine("Informe quantidade de KMs que sera percorida na sua viagem: ");
+            KM = Convert.ToInt32(Console.ReadLine());
+
+            if (KM < 200)
+            {
+                Console.WriteLine("Seu percuso custara o valor de {0}", (KM * 0.50));
+            }
+            else
+            {
+                Console.WriteLine("Seu percuso custara o valor de {0}", (KM * 0.45));
+            }
+        }
+    }
+}
