24544c9cb1b8178ecbf8f92d9ca9e30c14d9c5b9
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-20 22:05:24 -0300

    Salario do cidadão

---
+++ Ex0011/Ex0011/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex0011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+            
+            Console.WriteLine("informe o valor do seu salario atual: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+            
+            
+
+            if (salario > 2500)
+            {
+                Console.WriteLine("Seu salario sera de {0}! Aumento de 5% sobre o valor de {1}.", ((salario * 0.05)) + salario, salario);
+            }
+            else if (salario > 1250)
+            {
+                Console.WriteLine("Seu salario sera de {0}! Aumento de 10% sobre o valor de {1}.", ((salario * 0.10)) + salario, salario);
+            }
+            else
+            {
+                Console.WriteLine("Seu salario sera de {0}! Aumento de 15% sobre o valor de {1}.", ((salario * 0.15)) + salario, salario);
+            }
+
+        }
+    }
+}
