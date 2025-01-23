3762d6829ac2fad98f5cfa1f36c3c33917d4c4f6
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-22 21:02:40 -0300

    mais um dia 22/01

---
+++ ex0012/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ex0012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int final;
+            Console.WriteLine("   \t    ______________________,                         ");
+            Console.WriteLine("   /_________I               ()                 ");
+            Console.WriteLine(" \\    ___-     _____________/          ");
+            Console.WriteLine("  /           /__/ ");
+            Console.WriteLine(" /      /     ");
+            Console.WriteLine("/______/");
+            Console.WriteLine("digite o final do intervalo vacilão");
+            final = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= final; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("{0}," ,i);
+                }
+            }
+        }
+    }
+}
