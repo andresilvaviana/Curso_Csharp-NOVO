ea335f4b7da576c635f809ddab9ef6895cae247a
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-22 21:58:26 -0300

    validação

---
+++ ex16/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero;
+            
+            Console.Write("Digite um genero: ");
+            genero = Console.ReadLine().ToUpper();
+
+            while (genero != "F" && genero != "M")
+            {
+                Console.Write("Digite um genero: ");
+                genero = Console.ReadLine().ToUpper();
+
+            }
+            Console.WriteLine("Bem-Vindos e Bem-Vidas de C#!");
+        }
+    }
+}
