831b48989b24f239be3dfd47e26f02847f992b69
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-22 21:37:20 -0300

    tabuada

---
+++ ex15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int num1;
+
+            Console.Write("informe um numero para a multiplicação: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", num1, i, num1 * i);
+            }
+        }       
+    }
+}
