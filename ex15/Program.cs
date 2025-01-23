643805ef864e00f4aff331d1bfc6557ae85b886f
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-22 22:15:27 -0300

    novo

--- ex15/Program.cs
+++ ex15/Program.cs
@@ -6,11 +6,17 @@
         {
 
 
-            int num1;
+            string num;
 
             Console.Write("informe um numero para a multiplicação: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
+            num = Convert.ToInt32(Console.ReadLine());
 
+            while (!int.TryParse(num, out _))
+            {
+                Console.Write("Digite um numero valido: ");
+                num = Console.ReadLine();
+            }
+            
             for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine("{0} x {1} = {2}", num1, i, num1 * i);
