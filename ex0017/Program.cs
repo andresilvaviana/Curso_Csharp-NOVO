e4dd43062e47e518cabd3ba5d5167894842022de
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-27 18:57:48 -0300

    While e Do Whiler

---
+++ ex0017/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ex0017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5; // inicializador de variavel
+
+            /*
+            equanto a condição for verdadeira,
+            o bloco de condigo seré executado.
+            */
+
+            /*
+            while (num < 5)
+            {
+                Console.WriteLine("Curso C#");
+            }
+            */
+
+            do
+            {
+                Console.WriteLine("Curso C#");
+                num++;
+            } while (num < 5);
+        }
+    }
+}
