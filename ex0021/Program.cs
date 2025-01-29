d6b3faefc87c9b59b7cca160dd515a84f7a9e203
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-29 19:47:04 -0300

    metodos

---
+++ ex0021/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ex0021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //metodos
+
+            //soma(10, 4);
+            saudacao("André");
+        }
+        static void soma(int a, int b)
+        {
+            int resultado = a + b;
+            Console.WriteLine("a soma de " + a + " + " + b + " = " + resultado);
+        }
+        static void saudacao(string nome)
+        {
+            Console.WriteLine(nome + ". Seja Bem Vindo!");
+        }
+
+    }
+}
+           
