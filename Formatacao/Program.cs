d230d01c2c72edf369d62b0dc6722202f835adc3
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-29 20:18:04 -0300

    .

---
+++ Formatacao/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Formatacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 10.54821;
+            double num2 = 54781.65874;
+
+            Console.WriteLine($"{num1:F3}");
+            Console.WriteLine($"{num2:n2}");
+            Console.WriteLine($"{num1:c2}");
+            Console.WriteLine($"{num2:e2}");
+            Console.WriteLine($"{num1:p2}");
+        }
+    }
+}
