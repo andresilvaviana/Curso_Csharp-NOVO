f9729740abe125767cd121286438b8cd5b3acb45
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-29 20:10:09 -0300

    exercico

--- AreTerreno/Program.cs
+++ AreTerreno/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AreTerreno
+﻿using System.Formats.Asn1;
+
+namespace AreTerreno
 {
     internal class Program
     {
@@ -9,6 +11,22 @@
             Console.WriteLine("++++++++++++++++++++++++++");
             Console.WriteLine();
 
+            Console.WriteLine("Digite a largura do terreno:");
+            double l = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a comprimento do terreno:");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            are(l, c);
         }
+
+        static void are(double largura, double comprimento)
+        {
+            double a = largura * comprimento;
+            Console.WriteLine($" A area do terreno de {largura} por {comprimento} é de {a:F2}m²");
+        }
+
     }
 }
