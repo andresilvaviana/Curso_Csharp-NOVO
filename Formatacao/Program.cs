a459238c9ea5e7a1aaa76fc9bf4a0bd1da6acae1
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-29 20:19:33 -0300

    .

--- Formatacao/Program.cs
+++ Formatacao/Program.cs
@@ -7,11 +7,11 @@
             double num1 = 10.54821;
             double num2 = 54781.65874;
 
-            Console.WriteLine($"{num1:F3}");
-            Console.WriteLine($"{num2:n2}");
-            Console.WriteLine($"{num1:c2}");
-            Console.WriteLine($"{num2:e2}");
-            Console.WriteLine($"{num1:p2}");
+            Console.WriteLine($"{num1:F3}"); // 4 casa decimais
+            Console.WriteLine($"{num2:n2}"); // separador de milhar
+            Console.WriteLine($"{num1:c2}"); // moeda
+            Console.WriteLine($"{num2:e2}"); // notação científica
+            Console.WriteLine($"{num1:p2}"); // porcentagem
         }
     }
 }
