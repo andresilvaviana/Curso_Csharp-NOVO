33fed996baff5bb55213d416399fdf9da3ef79c2
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-29 19:49:52 -0300

    excercio metodos

---
+++ AreTerreno/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AreTerreno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("++++++++++++++++++++++++++");
+            Console.WriteLine("CALCULAR A AREA DO TERRENO");
+            Console.WriteLine("++++++++++++++++++++++++++");
+            Console.WriteLine();
+
+        }
+    }
+}
