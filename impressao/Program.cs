ba0f39c556d9a2b2d4a3a761a36753f7fd67aa20
andre s. viana <andre.silva.viana.1995@gmail.com>
2025-01-29 21:09:45 -0300

    impressao

---
+++ impressao/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace impressao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            escreva("André Silva Viana");
+            Console.WriteLine();
+            escreva("COCO Bambun");
+            Console.WriteLine();
+            escreva("Tarzan");
+        }
+        static void escreva(string texto)
+        {
+            char caracter = '*'; // caracter que será impresso
+            int tamanho = texto.Length + 4; // tamanho da linha
+            string linha = new string(caracter, tamanho); // linha com o caracter
+            string textocentralizado = texto.PadLeft(2 + texto.Length).PadRight(tamanho); // texto centralizado
+            Console.WriteLine(linha); // imprime a linha 
+            Console.WriteLine(textocentralizado); // imprime o texto centralizado 
+            Console.WriteLine(linha); // imprime a linha 
+            
+
+
+        }
+    }
+}
